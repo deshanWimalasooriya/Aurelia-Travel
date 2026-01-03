5b8f6b0631eac311080d86539d501d1e3f9a380b
deshanWimalasooriya <deshanwimalasooriya000@gmail.com>
2026-01-03 22:20:23 +0530

    update
    Create about and contact page

--- aurelia-travel-frontend/src/App.jsx
+++ aurelia-travel-frontend/src/App.jsx
@@ -11,6 +11,8 @@ import HotelDetails from './pages/HotelDetails'
 import LoginRegister from './pages/LoginRegister'
 import Profile from './pages/Profile'
 import TravelPage from './pages/TravelPage'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 import './index.css'
 
@@ -27,6 +29,8 @@ function App() {
           <Route path="/profile" element={user ? <Profile /> : <Navigate to="/auth" />} />
           <Route path="/auth" element={!user ? <LoginRegister /> : <Navigate to="/profile" />} />
           <Route path="/travel-plan" element={user ? <TravelPage /> : <Navigate to="/auth" />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Layout>
     </AuthProvider>
