ea9555eb0ecac2a3e18134da159b3b07f826f2dd
deshanWimalasooriya <deshanwimalasooriya000@gmail.com>
2026-02-17 03:28:43 +0530

    update

--- aurelia-travel-backend/API/routes/notificationRoutes.js
+++ aurelia-travel-backend/API/routes/notificationRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const notifController = require('../controllers/notificationController');
+const notifyController = require('../controllers/notificationController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 router.use(verifyToken);
 
 // Get all
-router.get('/', notifController.getMyNotifications);
+router.get('/', notifyController.getMyNotifications);
 
 // Mark single as read (This is the backend logic you requested)
-router.put('/:id/read', notifController.markRead);
+router.put('/:id/read', notifyController.markRead);
 
 // Mark all
-router.put('/read-all', notifController.markAllRead);
+router.put('/read-all', notifyController.markAllRead);
 
 module.exports = router;
